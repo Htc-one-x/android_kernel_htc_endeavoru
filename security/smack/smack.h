3abd5325e4f98df181dc4356124dabb1cfd52b38
DevUt <javitri61@gmail.com>
2016-09-18 18:21:48 +0530

    Revert "Whole fix for selinux"
    This reverts commit 80d7b05f6eeed32fdb586f0086142cd2ab05d229.

--- security/smack/smack.h
+++ security/smack/smack.h
@@ -180,15 +180,6 @@ struct smack_known {
  */
 #define SMK_NUM_ACCESS_TYPE 4
 
-/* SMACK data */
-struct smack_audit_data {
-	const char *function;
-	char *subject;
-	char *object;
-	char *request;
-	int result;
-};
-
 /*
  * Smack audit data; is empty if CONFIG_AUDIT not set
  * to save some stack
@@ -196,7 +187,6 @@ struct smack_audit_data {
 struct smk_audit_info {
 #ifdef CONFIG_AUDIT
 	struct common_audit_data a;
-	struct smack_audit_data sad;
 #endif
 };
 /*
@@ -313,18 +303,9 @@ void smack_log(char *subject_label, char *object_label,
 static inline void smk_ad_init(struct smk_audit_info *a, const char *func,
 			       char type)
 {
-	memset(&a->sad, 0, sizeof(a->sad));
+	memset(a, 0, sizeof(*a));
 	a->a.type = type;
-	a->a.smack_audit_data = &a->sad;
-	a->a.smack_audit_data->function = func;
-}
-
-static inline void smk_ad_init_net(struct smk_audit_info *a, const char *func,
-				   char type, struct lsm_network_audit *net)
-{
-	smk_ad_init(a, func, type);
-	memset(net, 0, sizeof(*net));
-	a->a.u.net = net;
+	a->a.smack_audit_data.function = func;
 }
 
 static inline void smk_ad_setfield_u_tsk(struct smk_audit_info *a,
@@ -350,7 +331,7 @@ static inline void smk_ad_setfield_u_fs_path(struct smk_audit_info *a,
 static inline void smk_ad_setfield_u_net_sk(struct smk_audit_info *a,
 					    struct sock *sk)
 {
-	a->a.u.net->sk = sk;
+	a->a.u.net.sk = sk;
 }
 
 #else /* no AUDIT */
